2e70958591edb1064e2fc07dc6e4d791133656d8
mannfred <mannfredboehm@gmail.com>
2024-01-08 16:00:07 -0800

    joining rosenberg data (biomes, bird groups) to jv data

--- R_scripts/sixletternames_for_rosenbergdata.R
+++ R_scripts/sixletternames_for_rosenbergdata.R
@@ -3,16 +3,21 @@ library(tidyverse)
 
 
 # import data from Rosenberg et al (2019): https://www.science.org/doi/10.1126/science.aaw1313
-rosen_data <- readr::read_csv(file=here("data/rosenberg_etal_2019_science.csv"))
+rosen_data <- 
+  readr::read_csv(file=here("data/rosenberg_etal_2019_science.csv")) %>% 
+  select(species, breeding_biome, bird_group)
+saveRDS(rosen_data, file=here("data/rds_files/rosenberg_data_filtered.rds"))
+
 
 # import JV data
-jv_data <- read_csv(file=here("data/JV_Percent_Bird_Populations_Combined.csv"))
+jv_data <- read_csv(file=here("data/rds_files/JV_Percent_Bird_Populations_Combined.csv"))
 
 # give Rosenberg data six-letter codes for species
 # by matching English names in the two datasets
 # note: only 495 of 529 species matched
 
-rosen_data <- dplyr::inner_join(rosen_data, jv_data, by="species")
+jv_rosen_data <- dplyr::left_join(jv_data, rosen_data, by="species") %>% 
+saveRDS(jv_rosen_data, file=here("data/rds_files/jv_data_with_birdgroups_and_biomes.rds"))
 
   
   
